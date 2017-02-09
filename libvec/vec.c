fb06ca2021fe10d9e6d79b7e6617cb3e7edc3bc3
Daichi Fukuoka <dc-fukuoka@sgi.com>
2017-02-09 11:34:43 +0900

    removed unnecessary main() in vec.c

--- libvec/vec.c
+++ libvec/vec.c
@@ -57,26 +57,3 @@ void vec_clear(vector_t *p)
 	free(p->_str);
 	free(p);
 }
-
-int main(int argc, char **argv)
-{
-	int i;
-	vector_t *p = NULL;
-	char *str;
-
-	if (argc == 1)
-		return -1;
-
-	for (i=1; i<argc; i++) {
-		p = vec_push(p, argv[i]);
-	}
-
-	for (i=p->_size-1; i>=0; i--) {
-		str = vec_pop(p);
-		printf("vec[%d]: address: %p: %s\n", i, p->_str[i], str);
-		free(str);
-	}
-	/* vec_show(p); */
-	vec_clear(p);
-	return 0;
-}
